2b00083e4cf4eff795ef1934521dc92c2a8fc3ef
Peter Tag <peter.h.tag@gmail.com>
2019-04-19 18:33:24 -0700

    Nearly final updates...

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
-USE burgers_db;
-DELETE FROM burgers;
 INSERT INTO burgers (burger_name, devoured) VALUES ('Texas Trooper', 0);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Holy Guacomole', 0);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Delight', 0);
